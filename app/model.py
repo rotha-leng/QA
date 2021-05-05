2f344f92f8c0f1a0ea52e52008842c87eee4831b
Narin <nariny.morakot@gmail.com>
2021-05-05 11:17:55 +0700

    updated

--- app/model.py
+++ app/model.py
@@ -7,14 +7,14 @@ from flask_login import UserMixin, login_user, login_required, logout_user, curr
 class MKT_USER(db.Model):
     ID = db.Column(db.Integer, primary_key=True)
     Fullname = db.Column(db.String(50))
-    Email_Address = db.Column(db.String(75))
+    Email = db.Column(db.String(75))
     Password = db.Column(db.String(75))
     Avatar = db.Column(db.String(100))
     Created = db.Column(db.String(50), default=datetime.datetime.utcnow)
 
     def __init__(self, FullName, Email_Address, Password, Avatar, Created):
         self.FullName = FullName
-        self.Email_Address = Email_Address
+        self.Email = Email
         self.Password = Password
         self.Avatar = Avatar
         self.Created = Created
--- app/question.py
+++ app/question.py
@@ -10,36 +10,59 @@ import sqlalchemy
 from sqlalchemy.sql.expression import cast
 
 
-#@app.route('/Post/<int:postid>')
-#@app.route('/Post/')
-@app.route('/')
-def getPost(postid=''):
-	"""Fetch all post records by author ID, or all
-	
-	Args:
-		postid (str, optional): if passed, ID of post to be displayed
-	
-	Returns:
-		post object: Post Object Record
-	"""
-	if postid=='':
-		questions = db.session.query(MKT_QUESTION.Title,MKT_QUESTION.Body,MKT_QUESTION.BestAnswer,MKT_QUESTION.Created).all()
-		question = db.session.\
-				query(MKT_QUESTION.ID, MKT_QUESTION.Title, MKT_QUESTION.Body, 
-				MKT_QUESTION.Tag, MKT_QUESTION.Vote, MKT_QUESTION.User, MKT_QUESTION.BestAnswer, MKT_QUESTION.Created,MKT_USER.Fullname).\
-				join(MKT_USER, cast(MKT_USER.ID, sqlalchemy.Numeric())==MKT_QUESTION.User).\
-				order_by(MKT_QUESTION.Created.desc()).\
-				all()
-		print(question)
-		print(questions)
-	else:
 
-		question = db.session.query(MKT_QUESTION.ID, MKT_QUESTION.Title, MTK_QUESTION.Body, 
-				MKT_QUESTION.Tag, MKT_QUESTION.Vote, MKT_QUESTION.User, MKT_QUESTION.BestAnswer, MKT_QUESTION.Created,MKT_User.Fullname).\
-				join(MKT_USER, cast(MKT_USER.ID, sqlalchemy.Numeric())==MKT_QUESTION.User).filter(MKT_QUESTION.ID == postid)
-		print(question)
-		if question.first() is None:
-			abort(404) # trigger 404 post not found
- 
+class searchQA(Form):
+    searchbox = TextField("Search",[validators.Required("Please Enter your Title!")])
+@app.route('/', methods=['POST', 'GET'])
+def getPost():
 
-	return render_template('home/index.html',posts=questions)
+
+
+    search = request.args.get('Search')
+    """Fetch all post records by author ID, or all
+    
+    Args:
+        postid (str, optional): if passed, ID of post to be displayed
+    
+    Returns:
+        post object: Post Object Record
+    """
+
+    if search== None:
+        totalQA = MKT_QUESTION.query.count()
+        questions = db.session.query(MKT_QUESTION.Title,MKT_QUESTION.Body,MKT_QUESTION.BestAnswer,MKT_QUESTION.Created).all()
+        question = db.session.\
+                query(MKT_QUESTION.ID, MKT_QUESTION.Title, MKT_QUESTION.Body, 
+                MKT_QUESTION.Tag, MKT_QUESTION.Vote, MKT_QUESTION.User, MKT_QUESTION.BestAnswer, MKT_QUESTION.Created,MKT_USER.Fullname).\
+                join(MKT_USER, cast(MKT_USER.ID, sqlalchemy.Numeric)==MKT_QUESTION.User).\
+                order_by(MKT_QUESTION.Created.desc()).\
+                all()
+        
+    else:
+        search = "%{}%.format"(search)
+        questions = db.session.query(MKT_QUESTION.Title,MKT_QUESTION.Body,MKT_QUESTION.BestAnswer,MKT_QUESTION.Created).all()
+        
+        question = db.session.query(MKT_QUESTION.ID, MKT_QUESTION.Title, MTK_QUESTION.Body, 
+                MKT_QUESTION.Tag, MKT_QUESTION.Vote, MKT_QUESTION.User, MKT_QUESTION.BestAnswer, MKT_QUESTION.Created,MKT_User.Fullname).\
+        join(MKT_USER, cast(MKT_USER.ID, sqlalchemy.Numeric)==MKT_QUESTION.User).\
+        filter(or_
+                (MKT_QUESTION.Title.like("%"f"{search}""%"),
+                MKT_QUESTION.Body.like("%"f"{search}""%"),
+                MKT_QUESTION.Tag.like("%"f"{search}""%")))
+        totalQA = question.count()
+    return render_template('home/index.html',question=question,totalQA=totalQA )
+        
+                    
+def btnFilter(condition):
+    if condition == None:
+        return redirect(url_for('getPost'))		
+    else:
+        questions = db.session.query(MKT_QUESTION.Title,MKT_QUESTION.Body,MKT_QUESTION.BestAnswer,MKT_QUESTION.Created).all()
+
+        question = db.session.query(MKT_QUESTION.ID, MKT_QUESTION.Title, MTK_QUESTION.Body, 
+                MKT_QUESTION.Tag, MKT_QUESTION.Vote, MKT_QUESTION.User, MKT_QUESTION.BestAnswer, MKT_QUESTION.Created,MKT_User.Fullname).\
+                join(MKT_USER, cast(MKT_USER.ID, sqlalchemy.Numeric)==MKT_QUESTION.User).\
+                filter(MKT_qQUESTION.Title.like("%Test1%"))
+        totalQA = question.count()
+        return render_template('home/index.html',question=question,totalQA=totalQA )
+    
